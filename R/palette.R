a4b5a97f48f33e7cecda2a8b5591947678644a81
nicoschoutteet <nico.schoutteet@creg.be>
2021-07-06 14:12:49 +0200

    Initial commit

---
+++ R/palette.R
@@ -0,0 +1,31 @@
+# Color palette ------------------------------------------------------------
+
+colour_vector_CREG <- c(
+  "blue1" = "#00B0B9",
+  "blue2" = "#005F83",
+  "blue3" = "#002D72",
+  "blue4" = "#00A9E0",
+  "purple" = "???485CC7",
+  "green" = "#78D64B",
+  "yellow" = "#EFDF00",
+  "orange" = "#FF8200",
+  "pink" = "#FB637E",
+  "red" = "#C5003E",
+  "grey1" = "#C8C9C7",
+  "grey2" = "#75787B",
+  "black" = "#111111"
+)
+
+#' Palette
+#'
+#' @param ...
+#'
+#' @return
+#' @export
+
+colours_CREG <- function(...) {
+  cols <- c(...)
+  if (is.null (cols))
+    return(CREG_colours_vector)
+  CREG_colours_vector[[cols]]
+}
---
+++ R/theme.R
@@ -0,0 +1,23 @@
+# ggplot theme ------------------------------------------------------------
+# either call explicitly in ggplot, via + theme_CREG()
+# or set at beginning of each session via theme_set(theme_CREG)
+
+#' CREG_theme()
+#'
+#' @return
+#' @export
+
+theme_CREG <- function() {
+  theme_classic() %+replace%
+    theme(plot.title = element_text(size = 9, hjust = 0, colour = CREG_colours("blue1")),
+          plot.subtitle = element_text(size = 6, hjust = 0, margin = margin(0, 0, .5, 0, "cm")),
+          plot.title.position = "plot",
+          plot.caption = element_text(hjust = 0 , size = 5),
+          plot.caption.position = "plot",
+          legend.position = "none",
+          legend.title = element_text(size = 6, face = "bold"),
+          legend.text = element_text(size = 6),
+          axis.title = element_text(size = 6),
+          axis.text = element_text(size = 6),
+          panel.grid.major = element_line(size = .25, color = "grey", linetype = "dotted"))
+}
