059414828bd806f680aadad463aea58addb9372d
nicoschoutteet <84858608+nicoschoutteet@users.noreply.github.com>
2021-07-07 10:11:39 +0200

    Update theme.R

--- R/theme.R
+++ R/theme.R
@@ -9,15 +9,15 @@
 
 theme_CREG <- function() {
   theme_classic() %+replace%
-    theme(plot.title = element_text(size = 9, hjust = 0, colour = colours_CREG("blue1")),
-          plot.subtitle = element_text(size = 6, hjust = 0, margin = margin(0, 0, .5, 0, "cm")),
+    theme(plot.title = element_text(size = 10, hjust = 0, colour = colours_CREG("blue1")),
+          plot.subtitle = element_text(size = 8, hjust = 0, margin = margin(0, 0, .5, 0, "cm")),
           plot.title.position = "plot",
           plot.caption = element_text(hjust = 0 , size = 5),
           plot.caption.position = "plot",
           legend.position = "none",
-          legend.title = element_text(size = 6, face = "bold"),
-          legend.text = element_text(size = 6),
-          axis.title = element_text(size = 6),
-          axis.text = element_text(size = 6),
+          legend.title = element_text(size = 8, face = "bold"),
+          legend.text = element_text(size = 8),
+          axis.title = element_text(size = 8),
+          axis.text = element_text(size = 8),
           panel.grid.major = element_line(size = .25, color = "grey", linetype = "dotted"))
 }
