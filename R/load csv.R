8dc0b0dc60b88f0f064a3b7400ff363971882984
nicoschoutteet <nico.schoutteet@creg.be>
2021-07-07 11:28:59 +0200

    update loadcsv_CREG

--- R/load csv.R
+++ R/load csv.R
@@ -1,15 +1,15 @@
 #' Import csv files which were extracted from DWH
-#' @description This function imports a csv file into an R session. 
+#' @description This function imports a csv file into an R session.
 #' The arguments include a reference to a subfolder of the working directory(filepath), a reference to the csv file (filename),
 #' and an indication of the type of column (in abbreviate form, e.g. "n" = numeric, "d" = double, "c" = character).
-#' @param filepath A string 
+#' @param subfoler A string
 #' @param filename A string
-#' @param datatypes A string
-#' @examples df <- loadcsv_CREG(filepath = "source data", filename = "loadBE", datatypes = "n")
-loadcsv_CREG <- function(filepath, filename, datatypes) {
-  read_delim(paste0(filepath, "/", filename), 
-             delim = ";", 
+#' @param col_types A string
+#' @examples df <- loadcsv_CREG(filepath = "source data", filename = "loadBE", col_types = "n")
+loadcsv_CREG <- function(subfolder, filename, col_types) {
+  read_delim(paste0(filepath, "/", filename, ".csv"),
+             delim = ";",
              locale = locale(decimal_mark = ","),
-             col_types = paste0("nnnccDcncncnncc", datatypes)) %>% 
+             col_types = paste0("nnnccDcncncnncc", col_types)) %>%
     mutate(DateTime = as.POSIXct(paste(YearMonthDayCSV, substr(Quarter, 1, 5)), tz = "Europe/Brussels"))
 }
