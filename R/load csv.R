bb9406c1f4958c2db1c001140ccb20a32c2e7db2
nicoschoutteet <nico.schoutteet@creg.be>
2021-07-07 11:34:50 +0200

    update loadcsv_CREG

--- R/load csv.R
+++ R/load csv.R
@@ -2,9 +2,9 @@
 #' @description This function imports a csv file into an R session.
 #' The arguments include a reference to a subfolder of the working directory(filepath), a reference to the csv file (filename),
 #' and an indication of the type of column (in abbreviate form, e.g. "n" = numeric, "d" = double, "c" = character).
-#' @param subfoler A string
-#' @param filename A string
-#' @param col_types A string
+#' @param subfoler A string, including the subfolder where the csv file is stored
+#' @param filename A stringn, including the name of hte csv file (don't include the extension ".csv" as this is added automatically)
+#' @param col_types A string describing, in short form, the column types of the data itself (i.e. not the year, month, day, etc. variables as those are added automatically)
 #' @examples df <- loadcsv_CREG(filepath = "source data", filename = "loadBE", col_types = "n")
 loadcsv_CREG <- function(subfolder, filename, col_types) {
   read_delim(paste0(subfolder, "/", filename, ".csv"),
