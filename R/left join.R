1401c0b47f2db8af0b4267737f1cd4d0a4d15246
nicoschoutteet <nico.schoutteet@creg.be>
2021-07-08 10:46:55 +0200

    add new left_join_CREG() function

--- R/left join.R
+++ R/left join.R
@@ -1,16 +1,16 @@
-#' Import csv files which were extracted from DWH
-#' @description This function imports a csv file into an R session.
+#' Left join csv files which were extracted from DWH
+#' @description This function left joints a csv file to another csv.
 #' The arguments include a reference to a subfolder of the working directory(filepath), a reference to the csv file (filename),
 #' and an indication of the type of column (in abbreviate form, e.g. "n" = numeric, "d" = double, "c" = character).
 #' @param subfoler A string, including the subfolder where the csv file is stored
 #' @param filename A stringn, including the name of hte csv file (don't include the extension ".csv" as this is added automatically)
 #' @param col_types A string describing, in short form, the column types of the data itself (i.e. not the year, month, day, etc. variables as those are added automatically)
-#' @examples df <- loadcsv_CREG(filepath = "source data", filename = "loadBE", col_types = "n")
+#' @examples df <- loadcsv_CREG(filepath = "source data", filename = "loadBE", col_types = "n") %>%
+#' left_join(filepath = "source data", filename = "loadNL", col_types = "n")
 
-loadcsv_CREG <- function(subfolder, filename, col_types) {
-  read_delim(paste0(subfolder, "/", filename, ".csv"),
-             delim = ";",
-             locale = locale(decimal_mark = ","),
-             col_types = paste0("nnnccDcncncnncc", col_types)) %>%
-    mutate(DateTime = as.POSIXct(paste(YearMonthDayCSV, substr(Quarter, 1, 5)), tz = "Europe/Brussels"))
+left_join_CREG <- function(subfolder, filename, col_types) {
+  left_join(select(loadCSV_CREG(subfolder, filename, col_types)),
+            DateTime, ncol:ncol-nchar(col_types),
+            by = c("DateTime" = "DateTime"),
+            keep = FALSE)
 }
