b2938afb3454e69f664cfd943972af3f159d7d4f
nicoschoutteet <nico.schoutteet@creg.be>
2021-07-08 17:11:17 +0200

    added new function for merged csv file import (loadmultiplecsv_CREG)

--- R/load csv.R
+++ R/load csv.R
@@ -2,8 +2,8 @@
 #' @description This function imports a csv file into an R session.
 #' The arguments include a reference to a subfolder of the working directory(filepath), a reference to the csv file (filename),
 #' and an indication of the type of column (in abbreviate form, e.g. "n" = numeric, "d" = double, "c" = character).
-#' @param subfoler A string, including the subfolder where the csv file is stored
-#' @param filename A stringn, including the name of hte csv file (don't include the extension ".csv" as this is added automatically)
+#' @param subfolder A string, including the subfolder where the csv file is stored
+#' @param filename A string, including the name of the csv file (don't include the extension ".csv" as this is added automatically)
 #' @param col_types A string describing, in short form, the column types of the data itself (i.e. not the year, month, day, etc. variables as those are added automatically)
 #' @examples df <- loadcsv_CREG(filepath = "source data", filename = "loadBE", col_types = "n")
 #' @export
@@ -14,4 +14,36 @@ loadcsv_CREG <- function(subfolder, filename, col_types) {
              locale = locale(decimal_mark = ","),
              col_types = paste0("nnnccDcncncnncc", col_types)) %>%
     mutate(DateTime = as.POSIXct(paste(YearMonthDayCSV, substr(Quarter, 1, 5)), tz = "Europe/Brussels"))
+}
+
+#' Import multiple csv files which were extracted from DWH and join them
+#' @description This function imports a csv file into an R session.
+#' The arguments include a reference to a subfolder of the working directory(filepath), a reference to the csv files (filenames),
+#' and an indication of the type of column (in abbreviate form, e.g. "n" = numeric, "d" = double, "c" = character).
+#' @param subfodler A string, including the subfolder where the csv file is stored
+#' @param filename A string vector, including the name of the csv file (don't include the extension ".csv" as this is added automatically) (e.g. c("loadBE", "loadNL"))
+#' @param col_types A string vector, describing, in short form, the column types of the data itself (i.e. not the year, month, day, etc. variables as those are added automatically) (e.g. c("nn", "nnnn"))
+#' @examples df <- loadmultiplecsv_CREG(filepath = "source data", filename = "loadBE", col_types = "n")
+#' @export
+
+loadmultiplecsv_CREG <- function(subfolder, filenames, col_types) {
+  tempfilepaths <<- paste0(subfolder, "/", filenames, ".csv")
+  tempcoltypes <<- paste0("nnnccDcncncnncc", col_types)
+  dataframelist <<- list()
+  for (i in 1) {
+    dataframelist[[i]] <<- read_delim(tempfilepaths[[i]],
+                                      delim = ";",
+                                      col_types = tempcoltypes[[i]],
+                                      locale = locale(decimal_mark = ",")) %>%
+      mutate(DateTime = as.POSIXct(paste(YearMonthDayCSV, substr(Quarter, 1, 5)), tz = "Europe/Brussels"))
+  }
+  for (i in seq(2, length(tempfilepaths))) {
+    dataframelist[[i]] <<- read_delim(tempfilepaths[[i]],
+                                      delim = ";",
+                                      col_types = tempcoltypes[[i]],
+                                      locale = locale(decimal_mark = ",")) %>%
+      mutate(DateTime = as.POSIXct(paste(YearMonthDayCSV, substr(Quarter, 1, 5)), tz = "Europe/Brussels")) %>%
+      select(DateTime, last_col(1:nchar(col_types[[i]])))
+  }
+  plyr::join_all(dataframelist, by = "DateTime", type = "left")
 }
