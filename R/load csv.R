9c367a79ab8db8f8124079d2f74c0da7d834a6b6
nicoschoutteet <nico.schoutteet@creg.be>
2021-07-14 12:12:27 +0200

    delete multiple csv loads

--- R/load csv.R
+++ R/load csv.R
@@ -2,80 +2,15 @@
 #' @description This function imports a csv file into an R session.
 #' The arguments include a reference to a subfolder of the working directory(filepath), a reference to the csv file (filename),
 #' and an indication of the type of column (in abbreviate form, e.g. "n" = numeric, "d" = double, "c" = character).
-#' @param subfolder A string, including the subfolder where the csv file is stored
-#' @param filename A string, including the name of the csv file (don't include the extension ".csv" as this is added automatically)
-#' @param col_types A string describing, in short form, the column types of the data itself (i.e. not the year, month, day, etc. variables as those are added automatically)
-#' @param resolution A string describing whether hourly ("h") or quarterly ("q") data are imported (default value = "h")
-#' @examples df <- loadcsv_CREG(filepath = "source data", filename = "loadBE", col_types = "n")
+#' @param filepath A string, including (if relevant) the subfolder where the csv file is stored (relative to the current working directory)
+#' @examples df <- loadcsv_CREG(filepath = "source data/loadBelgium.csv")
 #' @export
 
-loadcsv_CREG <- function(subfolder, filename, col_types, resolution = "h") {
-  if (resolution == "h") {
+loadcsv_CREG <- function(filepath) {
     read_delim(paste0(subfolder, "/", filename, ".csv"),
                delim = ";",
                locale = locale(decimal_mark = ","),
-               col_types = paste0("nnnccDcncncnncc", col_types)) %>%
+               guess_max = 1000000)) %>%
     mutate(DateTime = as.POSIXct(paste(YearMonthDayCSV, substr(Quarter, 1, 5)), format = "%Y-%m-%d %H:%M", tz = "Europe/Brussels")) %>%
     select(DateTime, everything())
-    } else if (resolution == "q") {
-    read_delim(paste0(subfolder, "/", filename, ".csv"),
-               delim = ";",
-               locale = locale(decimal_mark = ","),
-               col_types = paste0("nnnccDcncncnnncc", col_types)) %>%
-    mutate(DateTime = as.POSIXct(paste(YearMonthDayCSV, substr(Quarter, 1, 5)), format = "%Y-%m-%d %H:%M", tz = "Europe/Brussels")) %>%
-    select(DateTime, everything())
-    }
-}
-
-#' Import multiple csv files which were extracted from DWH and join them
-#' @description This function imports a csv file into an R session.
-#' The arguments include a reference to a subfolder of the working directory(filepath), a reference to the csv files (filenames),
-#' and an indication of the type of column (in abbreviate form, e.g. "n" = numeric, "d" = double, "c" = character).
-#' @param subfodler A string, including the subfolder where the csv file is stored
-#' @param filename A string vector, including the name of the csv file (don't include the extension ".csv" as this is added automatically) (e.g. c("loadBE", "loadNL"))
-#' @param col_types A string vector, describing, in short form, the column types of the data itself (i.e. not the year, month, day, etc. variables as those are added automatically) (e.g. c("nn", "nnnn"))
-#' @param resolution A string vector, describing whether hourly ("h") or quarterly ("q") data are inmported (e.g. "q")
-#' @examples df <- loadmultiplecsv_CREG(filepath = "source data", filename = c("loadBE", "loadNL"), col_types = c("n", "n"), quarterlyresolution = c("h", "h"))
-#' @export
-
-loadmultiplecsv_CREG <- function(subfolder, filenames, col_types, resolution) {
-  tempfilepaths <- paste0(subfolder, "/", filenames, ".csv")
-  tempcoltypesh <- paste0("nnnccDcncncnncc", col_types)
-  tempcoltypesq <- paste0("nnnccDcncncnnncc", col_types)
-  resolution <- resolution
-  dataframelist <- list()
-  for (i in 1) {
-    if (resolution[[i]] == "h") {
-      dataframelist[[i]] <- read_delim(tempfilepaths[[i]],
-                                       delim = ";",
-                                       col_types = tempcoltypesh[[i]],
-                                       locale = locale(decimal_mark = ",")) %>%
-      mutate(DateTime = as.POSIXct(paste(YearMonthDayCSV, substr(Quarter, 1, 5)), format = "%Y-%m-%d %H:%M", tz = "Europe/Brussels"))
-      } else if (resolution[[i]] == "q") {
-      dataframelist[[i]] <- read_delim(tempfilepaths[[i]],
-                                       delim = ";",
-                                       col_types = tempcoltypesq[[i]],
-                                       locale = locale(decimal_mark = ",")) %>%
-      mutate(DateTime = as.POSIXct(paste(YearMonthDayCSV, substr(Quarter, 1, 5)), format = "%Y-%m-%d %H:%M", tz = "Europe/Brussels"))
-      }
-  }
-  for (i in seq(2, length(tempfilepaths))) {
-    if (resolution[[i]] == "h") {
-      dataframelist[[i]] <- read_delim(tempfilepaths[[i]],
-                                       delim = ";",
-                                       col_types = tempcoltypesh[[i]],
-                                       locale = locale(decimal_mark = ",")) %>%
-      mutate(DateTime = as.POSIXct(paste(YearMonthDayCSV, substr(Quarter, 1, 5)), format = "%Y-%m-%d %H:%M", tz = "Europe/Brussels")) %>%
-      select(DateTime, last_col(1:nchar(col_types[[i]])))
-      } else if (resolution[[i]] == "q") {
-      dataframelist[[i]] <- read_delim(tempfilepaths[[i]],
-                                       delim = ";",
-                                       col_types = tempcoltypesq[[i]],
-                                       locale = locale(decimal_mark = ",")) %>%
-      mutate(DateTime = as.POSIXct(paste(YearMonthDayCSV, substr(Quarter, 1, 5)), format = "%Y-%m-%d %H:%M", tz = "Europe/Brussels")) %>%
-      select(DateTime, last_col(1:nchar(col_types[[i]])))
-      }
-  }
-  plyr::join_all(dataframelist, by = "DateTime", type = "left") %>%
-  select(DateTime, everything())
 }
