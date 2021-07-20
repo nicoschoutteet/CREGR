85e5d948756187d4286a451ffa317bd628ab795a
nicoschoutteet <nico.schoutteet@creg.be>
2021-07-20 11:36:01 +0200

    Update theme.R

--- R/theme.R
+++ R/theme.R
@@ -8,7 +8,7 @@
 
 theme_CREG <- function() {
   theme_classic() %+replace%
-    theme(plot.title = element_text(size = 10, hjust = 0, colour = colour_CREG("blue1")),
+    theme(plot.title = element_text(size = 10, hjust = 0, colour = colour_CREG_function("blue1")),
           plot.subtitle = element_text(size = 8, hjust = 0, margin = margin(0, 0, .5, 0, "cm")),
           plot.title.position = "plot",
           plot.caption = element_text(hjust = 0 , size = 5),
