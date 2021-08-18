59f05e56af01c4f94bd0db7489b57e886cf52972
nicoschoutteet <nico.schoutteet@creg.be>
2021-08-18 15:10:57 +0200

    include ggtext

--- R/theme.R
+++ R/theme.R
@@ -8,16 +8,16 @@
 
 theme_CREG <- function() {
   theme_classic() %+replace%
-    theme(plot.title = element_text(size = 10, hjust = 0, colour = colour_CREG_function("blue1")),
-          plot.subtitle = element_text(size = 8, hjust = 0, margin = margin(0, 0, .5, 0, "cm")),
+    theme(plot.title = element_markdown(size = 10, hjust = 0, colour = colour_CREG_function("blue1")),
+          plot.subtitle = element_markdown(size = 8, hjust = 0, margin = margin(0, 0, .5, 0, "cm")),
           plot.title.position = "plot",
-          plot.caption = element_text(hjust = 0 , size = 5),
+          plot.caption = element_markdown(hjust = 0 , size = 5),
           plot.caption.position = "plot",
           plot.margin = margin(0, 0, 0, 0, "cm"),
           legend.position = "none",
-          legend.title = element_text(size = 8, face = "bold"),
-          legend.text = element_text(size = 8),
-          axis.title = element_text(size = 8),
-          axis.text = element_text(size = 8),
+          legend.title = element_markdown(size = 8, face = "bold"),
+          legend.text = element_markdown(size = 8),
+          axis.title = element_markdown(size = 8),
+          axis.text = element_markdown(size = 8),
           panel.grid.major = element_line(size = .25, color = "grey", linetype = "dotted"))
 }
