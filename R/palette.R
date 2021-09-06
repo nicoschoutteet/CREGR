56d0e6f12faa51f611fe0b8caf92c15ab4ec31d0
nicoschoutteet <nico.schoutteet@creg.be>
2021-09-06 11:00:56 +0200

    Update palette.R

--- R/palette.R
+++ R/palette.R
@@ -18,20 +18,20 @@ colour_vector_CREG <- c(
 
 #' Colour picker
 #'
-#' @param:
+#' @examples:
 #' "blue1" = "#00B0B9",
-# "blue2" = "#005F83",
-#"blue3" = "#002D72",
-#"blue4" = "#00A9E0",
-#"purple" = "#485CC7",
-#"green" = "#78D64B",
-#"yellow" = "#EFDF00",
-#"orange" = "#FF8200",
-#"pink" = "#FB637E",
-#"red" = "#C5003E",
-#"grey1" = "#C8C9C7",
-#"grey2" = "#75787B",
-#"black" = "#111111"
+#' "blue2" = "#005F83",
+#' "blue3" = "#002D72",
+#' "blue4" = "#00A9E0",
+#' "purple" = "#485CC7",
+#' "green" = "#78D64B",
+#' "yellow" = "#EFDF00",
+#' "orange" = "#FF8200",
+#' "pink" = "#FB637E",
+#' "red" = "#C5003E",
+#' "grey1" = "#C8C9C7",
+#' "grey2" = "#75787B",
+#' "black" = "#111111"
 #'
 #' @return
 #' @export
